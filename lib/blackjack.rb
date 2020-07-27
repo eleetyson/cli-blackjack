cfcb0dcbdfeea5c382cd50d5fccf90c93196a757
Ethan <eleetyson@gmail.com>
2020-07-27 10:28:31 -0400

    change Blackjack instance methods to class methods

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,42 +1,50 @@
 class Blackjack
-  attr_accessor :deck_id, :deck, :player, :dealer
+  # attr_accessor :deck_id, :deck, :player, :dealer
+  attr_reader :deck_id, :deck, :player, :dealer
 
-  # @@all = []
+  @@all = []
 
   def initialize(deck_id, deck)
     @deck_id = deck_id
     @deck = deck
     @player = []
     @dealer = []
-    # self.save
+    self.save
   end
 
-  # def save
-  #   self.class.all << self
-  # end
-  #
-  # def self.all
-  #   @@all
-  # end
-
-  # draw a card for the player (pick a random card hash from the @deck array and add it to the player's hand -- @player)
-  def player_draw
-    self.player << self.deck.sample
+  # store the current instance in the @@all array
+  def save
+    self.class.all << self
   end
 
-  # draw a card for the dealer (pick a random card hash from the @deck array and add it to the dealer's hand -- @dealer)
-  def dealer_draw
-    self.dealer << self.deck.sample
+  # draw a card for the player (pick a random card hash from the @deck array and add it to the player's hand)
+  def self.player_draw
+    self.all.player << self.deck.sample
   end
 
-  # discard the player's hand (empty the @player array)
-  def player_discard
-    self.player.clear
+  # draw a card for the dealer (pick a random card hash from the @deck array and add it to the dealer's hand)
+  def self.dealer_draw
+    self.all.dealer << self.deck.sample
   end
 
-  # discard the dealer's hand (empty the @dealer array)
-  def dealer_discard
-    self.dealer.clear
+  # discard the player's hand (empty the player array in @@all)
+  def self.player_discard
+    self.all.player.clear
+  end
+
+  # discard the dealer's hand (empty the dealer array in @@all)
+  def self.dealer_discard
+    self.all.dealer.clear
+  end
+
+  # class method that references the @@all array in a more abstract, flexible way
+  def self.all
+    @@all
+  end
+
+  # empty out the @@all array
+  def self.empty
+    self.all.clear
   end
 
 end
