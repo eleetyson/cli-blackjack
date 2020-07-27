067c22ea2ddb4260a3cc062a34aaaea248f00df4
Ethan <eleetyson@gmail.com>
2020-07-27 09:39:54 -0400

    add methods to simulate drawing cards

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,11 +1,13 @@
 class Blackjack
-  attr_accessor :deck_id, :cards
+  attr_accessor :deck_id, :deck, :player, :dealer
 
   # @@all = []
 
-  def initialize(deck_id, cards)
+  def initialize(deck_id, deck)
     @deck_id = deck_id
-    @cards = cards
+    @deck = deck
+    @player = []
+    @dealer = []
     # self.save
     binding.pry
   end
@@ -17,5 +19,15 @@ class Blackjack
   # def self.all
   #   @@all
   # end
+
+  # draw a random card (a hash) from the deck and add it to the player's hand
+  def player_draw
+    self.player << self.deck.sample
+  end
+
+  # draw a random card (a hash) from the deck and add it to the dealer's hand
+  def dealer_draw
+    self.dealer << self.deck.sample
+  end
 
 end
