8b44a186a1caadad9c54e3dee01bccce4f239229
Ethan <eleetyson@gmail.com>
2020-07-27 10:01:25 -0400

    create greeting and start menu methods  in CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,12 +1,36 @@
 class CLI
 
   def greeting
-    puts "Welcome"
+    puts "------------------------"
+    puts "Welcome to CLI Blackjack"
+    puts "------------------------"
     API.get_new_deck
   end
 
   def start_menu
+    print "Enter 'y' to play or 'n' to exit: "
+    input = gets.strip.upcase
+
+    if input == "Y"
+      self.play
+    elsif input == "N"
+      self.exit
+    else
+      puts "* please enter either 'y' or 'n' *"
+      self.start_menu
+    end
+
+  end
+
+  def play
     
+    puts "------------------------"
+    puts "play functionality"
+  end
+
+  def exit
+    puts "------------------------"
+    puts "Bye!"
   end
 
 end
